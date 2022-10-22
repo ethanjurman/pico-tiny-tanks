239a3ee18278cfa09bb4e54a6a92943a722d1b09
Ethan Jurman <ethanjurman@gmail.com>
2022-10-22 11:50:48 -0400

    comments

--- client.js
+++ client.js
@@ -2,22 +2,26 @@ const socket = io();
 
 let playerId = null;
 
+// every 250ms check the room code from the last gpio values.
 let roomCodeInterval = setInterval(() => {
-  console.log()
   if (window.pico8_gpio[126] !== undefined && window.pico8_gpio[127] !== undefined) {
-    console.log(window.pico8_gpio[126], window.pico8_gpio[127])
     clearInterval(roomCodeInterval);
     socket.emit('room_join', { roomId: `${`${window.pico8_gpio[126]}`.padStart(2, '0')}${`${window.pico8_gpio[127]}`.padStart(2, '0')}` });
   }
-}, 30);
+}, 250);
 
+// on every frame send updates to the server about our tank from gpio
 function onFrameUpdate() {
+  // check to see if we have selected a tank
   if (window.pico8_gpio[0] !== undefined && window.pico8_gpio[0] !== 100 && playerId === null) {
     playerId = window.pico8_gpio[0];
   }
+  // check to see if the tank data in gpio is our tank
   if (playerId === window.pico8_gpio[0]) {
+    // if it is, send data to server (volatile means unsent data can be dropped)
     socket.volatile.emit("tank_update", window.pico8_gpio.slice(0, 40));
   }
+  // run this function 15ms from now, on the next animation frame (throttle how quickly data is sent / drawn)
   setTimeout(() => {
     window.requestAnimationFrame(onFrameUpdate);
   }, 15)
@@ -25,8 +29,13 @@ function onFrameUpdate() {
 
 window.requestAnimationFrame(onFrameUpdate);
 
+// when we get other tank data from the server, set our gpio so pico8 can read it
 socket.on('tank_update_from_server', (updatedTankData) => {
+  // if we haven't picked a player (we are not in a game), then return immediately
   if (playerId === null) { return; }
+  // if the updated tank data is our tank, then we can ignore the updates
+  if (playerId === updatedTankData[0]) { return; }
+  // write the updated tank data (which should exist in the first 40 values in gpio) to pico8
   for (let i = 0; i < 40; i++) {
     window.pico8_gpio[i] = updatedTankData[i];
   }
--- server/server.js
+++ server/server.js
@@ -6,25 +6,26 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+// host the static files (tiny-tanks.html, etc...)
 app.use(express.static(path.join(__dirname, '../')));
-
+// default any route to tiny-tanks.html
 app.use((__req, res) => res.sendFile(path.join(__dirname, '../tiny-tanks.html')));
 
+// host on port 5000, or if being served by a service, they can choose via process.env.PORT
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`Server Running\nhttp://localhost:${PORT}`));
+server.listen(PORT, () => console.log(`Server Running \nhttp://localhost:${PORT}`));
 
+// socket is a connection between the server and the client
 io.on('connection', (socket) => {
   let roomId;
-  socket.on('disconnect', () => {
-    console.log('disconnected');
-  });
+  socket.on('disconnect', () => { });
+  // attach a room id to the socket connection
   socket.on('room_join', (evtData) => {
-    console.log(evtData.roomId);
     socket.join(evtData.roomId);
     roomId = evtData.roomId;
   })
+  // when the server recives an update from the client, send it to every client with the same room id
   socket.on('tank_update', (updatedTankData) => {
-    console.log({ roomId })
     socket.to(roomId).volatile.emit('tank_update_from_server', updatedTankData);
   });
 });
